126b3b1c6de18ff87ca01124049d3fc056d9d9c6
Bychok300 <bychok300@gmail.com>
2016-12-19 18:55:23 +0300

    preReliz

---
+++ AndroidStudioProjects/SimpleParser_android/app/src/main/java/com/anton/src/simpleparser_android/About.java
@@ -0,0 +1,17 @@
+package com.anton.src.simpleparser_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+
+/**
+ * Created by rem0tec0de on 19.12.16.
+ */
+public class About extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
